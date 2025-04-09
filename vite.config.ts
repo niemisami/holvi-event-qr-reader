0e5d26420f3ceb6eb9127069d208fbd5b7afb585
niemisami <sami@nieminen.fi>
2025-04-09 20:04:31 +0300

    Initial vite-pwa setup

---
+++ vite.config.ts
@@ -0,0 +1,39 @@
+import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), VitePWA({
+    registerType: 'autoUpdate',
+    injectRegister: false,
+    devOptions: {
+      enabled: false
+    },
+
+    pwaAssets: {
+      disabled: false,
+      config: true,
+    },
+
+    manifest: {
+      name: 'holvi-event-reader',
+      short_name: 'holvi-event-reader',
+      description: 'holvi-event-reader',
+      theme_color: '#721378',
+    },
+
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+      cleanupOutdatedCaches: true,
+      clientsClaim: true,
+    },
+
+    devOptions: {
+      enabled: false,
+      navigateFallback: 'index.html',
+      suppressWarnings: true,
+      type: 'module',
+    },
+  })],
+})
